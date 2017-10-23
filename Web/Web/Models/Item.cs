e1697cde7786a29c2d58a1ad872ae8a2bd696ca3
1606404 <1606404@034LAB3010>
2017-10-23 15:43:15 -0200

    Criando as Models!

---
+++ Web/Web/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Web.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public int Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Raridade { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        //Relacionamento item -= Tipo Item
+
+        public int TipoItemID { get; set; }
+
+
+        public virtual TipoItem _TipoItem{ get; set; }
+
+    } 
+}
