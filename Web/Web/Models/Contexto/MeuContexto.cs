bb59936881f7f4e7427da957f4d08065c24ec30e
1606404 <1606404@034LAB3010>
2017-10-23 15:34:06 -0200

    Criando o Contexto!

---
+++ Web/Web/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Data.Entity;
+
+namespace Web.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+
+    }
+}
